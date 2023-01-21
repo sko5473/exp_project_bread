66c5a4abe20f4a8dbaafc5d3baad98ba295151de
sko5473 <skl5473@naver.com>
2023-01-22 00:07:57 +0900

    2023-01-22
    1개 상점 내 리뷰, 사진 모달창

--- models/bakeryreviewmodel.js
+++ models/bakeryreviewmodel.js
@@ -4,10 +4,11 @@ var sequence = require('mongoose-sequence')(mongoose);
 var BakeryReviewSchema = new mongoose.Schema({
     _id     : { type : Number, default : 0 }, // 글번호, 기본키
     bakery_id     : { type : Number, default : 0 }, // 빵집번호
+    writer : { type : String, default : ''}, //작성자
     point : { type : Number, default : '0'}, //평점
     content : { type : String, default : ''}, //내용
     regdate : { type : Date,   deafult : Date.now}, //등록일자
-    regdate1 : { type : Date,   deafult :''}, //등록일자 포맷 변경
+    regdate1 : { type : String,   deafult :''}, //등록일자 포맷 변경
     filedata : { type : Buffer, default : null}, //파일데이터
     filename : { type : String, default : '' }, //파일명
     filetype : { type : String, default : '' }, //파일종류
--- routes/bakeryreview.js
+++ routes/bakeryreview.js
@@ -24,6 +24,7 @@ router.post('/insertshopreview.json', upload.single("file"), async function (req
 
     const bakeryreview = new BakeryReview();
     bakeryreview.bakery_id = req.query._id;
+    bakeryreview.writer = req.body.writer;
     bakeryreview.point = req.body.point;
     bakeryreview.content = req.body.content;
     bakeryreview.filedata = req.file.buffer;
@@ -45,21 +46,25 @@ router.post('/insertshopreview.json', upload.single("file"), async function (req
 });
 
 //상점페이지 안 리뷰데이터 수신
-// 리뷰등록 => 127.0.0.1:3000/api/bakeryreview/selectreview.json?bakery_id=15
+//127.0.0.1:3000/api/bakeryreview/selectreview.json?page=1&bakery_id=15
 router.get('/selectreview.json', async function (req, res, next) {
   try {
-    const bakery_id = req.query._id;
+
+    const bakery_id = req.query.bakery_id;
     const page = req.query.page; //1
+    console.log('리퀘스트 쿼리', req.query);
+    console.log('리퀘스트', req);
 
     //전체 데이터에서 제목이 검색어가 포함된 것 가져오기
     // a => a123, 
-    const query = { bakery_id: new RegExp(bakery_id, 'i') }; //RegExp(포함된 것을 찾아내는 함수)
+    const query = { bakery_id: Number(bakery_id) }; //RegExp(포함된 것을 찾아내는 함수)
     const project = {
       filedata: 0,
       filename: 0,
       filesize: 0,
       filetype: 0,
     };
+
     const result = await BakeryReview.find(query, project)
       .sort({ _id: -1 })
       .skip((page - 1) * 10)
@@ -68,7 +73,7 @@ router.get('/selectreview.json', async function (req, res, next) {
     //등록일, 이미지URL 수동 생성                          
     for (let tmp of result) {
       //format("YYYY-MM-DD HH:mm:ss")
-      tmp.regdate1 = moment(tmp.regdate).format("YYYY-MM-DD");
+      tmp.regdate1 = moment(tmp.regdate).format("YYYY-MM-DD HH:mm:ss");
       tmp.imageurl = `/api/bakeryreview/image?_id=${tmp._id}&ts=${Date.now()}`;
     }
 
@@ -76,6 +81,79 @@ router.get('/selectreview.json', async function (req, res, next) {
     const total = await BakeryReview.countDocuments(query);
 
     return res.send({ status: 200, total: total, result: result });
+  } catch (e) {
+    console.error(e);
+
+    return res.send({ status: -1, result: e });
+  }
+});
+
+//모달창 내 1개 리뷰정보 수신
+router.get('/selectreviewone.json', async function (req, res, next) {
+  try {
+
+    const _id = req.query._id;
+    const bakery_id = req.query.bakery_id;
+
+    //전체 데이터에서 제목이 검색어가 포함된 것 가져오기
+    // a => a123, 
+    const query = { _id: Number(_id) }; //RegExp(포함된 것을 찾아내는 함수)
+    const query3 = { bakery_id: Number(bakery_id) };
+    const project = {
+      filedata: 0,
+      filename: 0,
+      filesize: 0,
+      filetype: 0,
+    };
+
+    const result = await BakeryReview.findOne(query,project);
+
+    //등록일, 이미지URL 수동 생성                          
+    if (result !== null) {
+      //format("YYYY-MM-DD HH:mm:ss")
+      result.regdate1 = moment(result.regdate).format("YYYY-MM-DD HH:mm:ss");
+      result.imageurl = `/api/bakeryreview/image?_id=${_id}&ts=${Date.now()}`;
+
+
+      //이전글
+      // $lt 작다, $gt 크다, $lte 작거나 같다, $gte 크거나 같다.
+      const query1 = { _id: { $lt: _id } };
+      const project1 = { _id: 1 };
+      let prev = await BakeryReview.find(query1, project1)
+        .sort({ _id: -1 })
+        .limit(1);
+      console.log('prev=>', prev);
+
+      if (prev.length > 0) {
+        prev = prev[0]._id;
+      }
+      else {
+        prev = 0;
+      }
+
+      //다음글
+      const query2 = { _id: { $gt: _id } };
+      let next = await BakeryReview.find(query2, project1)
+        .sort({ _id: 1 })
+        .limit(1);
+
+      console.log('NEXT=>', next);
+
+      if (next.length > 0) {
+        console.log('확인', next[0]._id);
+        next = next[0]._id;
+      }
+      else {
+        next = 0;
+      }
+
+      return res.send({
+        status: 200,
+        result: result,
+        prev: prev,
+        next: next,
+      });
+    } return res.send({ status: 0 });
   } catch (e) {
     console.error(e);
 
